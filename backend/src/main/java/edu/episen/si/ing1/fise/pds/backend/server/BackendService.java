5318a9aef33dc2f513acb1928d4eff4234950580
kanoute-coumba <kanoute.coumba@gmail.com>
2021-09-14 22:45:23 +0200

    Creation of shell script

---
+++ backend/src/main/java/edu/episen/si/ing1/fise/pds/backend/server/BackendService.java
@@ -0,0 +1,13 @@
+package edu.episen.si.ing1.fise.pds.backend.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BackendService {
+
+    private final static Logger logger = LoggerFactory.getLogger(BackendService.class.getName());
+    public static void main (String[] args) {
+        logger.info("Backend Service is Running...");
+
+    }
+}
---
+++ backend/src/main/scripts/backend-service.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ../../../target
+exec java -jar backend-1.0-SNAPSHOT-jar-with-dependencies.jar $*
