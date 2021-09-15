7c427ded8117bc034605a64b47273ff58867c6f7
kanoute-coumba <kanoute.coumba@gmail.com>
2021-09-15 21:54:01 +0200

    Add commanlines and options

--- backend/src/main/java/edu/episen/si/ing1/fise/pds/backend/server/BackendService.java
+++ backend/src/main/java/edu/episen/si/ing1/fise/pds/backend/server/BackendService.java
@@ -1,13 +1,35 @@
 package edu.episen.si.ing1.fise.pds.backend.server;
 
+import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BackendService {
 
     private final static Logger logger = LoggerFactory.getLogger(BackendService.class.getName());
-    public static void main (String[] args) {
-        logger.info("Backend Service is Running...");
+    public static void main (String[] args) throws ParseException {
+
+        final Options options = new Options();
+        final Option testMode = Option.builder().longOpt("testMode").build();
+        final Option maxConnection = Option.builder().longOpt("maxConnection").
+                hasArg().argName("maxConnection").build();
+        options.addOption(testMode);
+        options.addOption(maxConnection);
+
+        final CommandLineParser parser = new DefaultParser();
+        final CommandLine commandLine = parser.parse(options,  args);
+
+        boolean inTestMode  = false;
+        int maxConnectionValue = 10;
+
+        if(commandLine.hasOption("testMode")) {
+            inTestMode = true;
+        }
+        if(commandLine.hasOption("maxConnection")) {
+            maxConnectionValue = Integer.parseInt(commandLine.getOptionValue("maxConnection"));
+        }
+
+        logger.info("Backend Service is Running...(testMode={}, maxConnection={})...", inTestMode, maxConnectionValue);
 
     }
 }
