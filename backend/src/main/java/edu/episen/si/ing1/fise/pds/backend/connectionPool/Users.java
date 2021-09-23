e78a7ac199d1beb03a7fdbb05b7bda5af5772ebe
kanoute-coumba <kanoute.coumba@gmail.com>
2021-09-23 23:12:29 +0200

    script for crud operations

---
+++ backend/src/main/java/edu/episen/si/ing1/fise/pds/backend/connectionPool/Users.java
@@ -0,0 +1,22 @@
+package edu.episen.si.ing1.fise.pds.backend.connectionPool;
+
+public class Users {
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {return this.id;}
+    public String getName() {return this.name;}
+    public int getAge() {return this.age;}
+    public void setName(String n) {this.name=n;}
+    public void setAge(int a) {this.age=a;}
+
+    public Users()
+    {}
+    public Users(int id, String name,int age)
+    {
+        this.id=id;
+        this.name=name;
+        this.age=age;
+    }
+}
