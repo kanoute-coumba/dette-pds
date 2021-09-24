afca91a48365ff5d17fc4cc3228c5793c6726405
kanoute-coumba <kanoute.coumba@gmail.com>
2021-09-24 18:04:57 +0200

    Create server configurations

---
+++ backend/src/main/java/edu/episen/si/ing1/fise/pds/backend/connectionPool/Config.java
@@ -0,0 +1,53 @@
+package edu.episen.si.ing1.fise.pds.backend.connectionPool;
+
+
+public class Config {
+    private String driver;
+    private String url;
+    private String username;
+    private String password;
+
+    private String create;
+    private String read;
+    private String update;
+    private String delete;
+    private int listenPort;
+    private int soTimeout;
+
+    private String ip;
+    public String getIp() {
+        return ip;
+    }
+    public int getListenPort() {
+        return listenPort;
+    }
+    public int getSoTimeout() {
+        return soTimeout;
+    }
+    public String getCreate() {
+        return create;
+    }
+    public String getRead() {
+        return read;
+    }
+    public String getUpdate() {
+        return update;
+    }
+    public String getDelete() {
+        return delete;
+    }
+    //who have acces read only
+    public String getDriver() {
+        return driver;
+    }
+    public String getURL() {
+        return url;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+}
---
+++ backend/src/main/java/edu/episen/si/ing1/fise/pds/backend/server/ServerConfig.java
@@ -0,0 +1,34 @@
+package edu.episen.si.ing1.fise.pds.backend.server;
+
+import edu.episen.si.ing1.fise.pds.backend.connectionPool.Config;
+import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+
+
+public class ServerConfig {
+    private final static Logger logger = LoggerFactory.getLogger(ServerConfig.class.getName());
+    private static final String episenServerConfigEnVar = "SMART_CITY";
+    private final String episenServerConfigFileLocation;
+    private Config config;
+
+
+    public Config getConfig() {
+        return config;
+    }
+
+
+    public ServerConfig() throws IOException {
+        this.episenServerConfigFileLocation = System.getenv(episenServerConfigEnVar);
+        logger.debug("Config file = {} ", episenServerConfigFileLocation);
+        final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        config = mapper.readValue(new File(episenServerConfigFileLocation), Config.class);
+
+
+    }
+}
