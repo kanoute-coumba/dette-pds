99d5b4134d1649b1f3461d362e9aa00973764453
kanoute-coumba <kanoute.coumba@gmail.com>
2021-09-27 16:12:05 +0200

    Update BackendService

--- backend/src/main/java/edu/episen/si/ing1/fise/pds/backend/server/BackendService.java
+++ backend/src/main/java/edu/episen/si/ing1/fise/pds/backend/server/BackendService.java
@@ -1,33 +1,121 @@
 package edu.episen.si.ing1.fise.pds.backend.server;
 
+import edu.episen.si.ing1.fise.pds.backend.connectionPool.ConnectionDB;
 import edu.episen.si.ing1.fise.pds.backend.connectionPool.DataSource;
 import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class BackendService extends Thread {
+    private final static Logger logger = LoggerFactory.getLogger(BackendService.class.getName());
 
     public static DataSource ds=new DataSource(5,5);
-    //ServerSocket server;
-
+    ServerSocket server;
     Socket client;
     public static ServerConfig serverConfig;
-    boolean m_bRunThread = true;
     public static  int max_connection_i = 0, connection_duration_i = 0;
 
+    public BackendService(final ServerConfig config) {
+        try {
+            server = new ServerSocket(config.getConfig().getListenPort());
 
-    static ServerSocket myServerSocket;
-    static boolean ServerOn = true;
-    public BackendService(Socket clientSocket) {
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-        client=clientSocket;
+    }
+    public void run()
+    {
+        PrintWriter out=null;
+        BufferedReader in=null;
+        this.serve();
+        while(ds.getUsedConnection()<max_connection_i )
+        {
+
+            try {
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                String operation_name=in.readLine();
+                if(operation_name!=null)
+                {
+                    CrudOperation(operation_name);
+                    out = new PrintWriter(client.getOutputStream(),true);
+                    out.println(CrudOperation(operation_name));
+                    System.out.print("connexion number "+(ds.getUsedConnection()+1)+" asking for a/an ");
+                    ds.setUsedConnection(ds.getUsedConnection()+1);
+                    //interval between each connexion
+                    sleep(connection_duration_i*1000);
+                    if(ds.getUsedConnection()>=max_connection_i ) {
+
+                        out.println("Server is occupied!");
+                    }
+                }
+                System.out.print("***************************************************************\n ");
+            } catch (Exception e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+
+        }
+        try {
+            in.close();
+            out.close();
+            server.close();
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public String CrudOperation(String operation_name) throws Exception
+    {
+        ConnectionDB c = ds.takeCon();
+        System.out.println(operation_name + " operation :");
+        String result="";
+        switch (operation_name) {
+            case "add":
+                //Add operation
+                result= c.createPerson();
+                break;
+            case "update":
+                // Update operation
+                result= c.updatePerson();
+                break;
+            case "delete":
+                //Delete operation
+                result= c.deletePerson();
+                break;
+            case "read":
+                //Read operation
+                result=c.listPerson().toString();
+                break;
+            default:
+                result="operation non-existent!!";
+                break;
+        }
+        c.connection.close();
+        return result;
     }
 
-    private final static Logger logger = LoggerFactory.getLogger(BackendService.class.getName());
-    public static void main (String[] args) throws ParseException {
+    public void serve() {
+        try {
+            client= server.accept();
+            logger.debug("a client has been detected !!");
+            //    final ClientRequestManager clientRequestManager = new ClientRequestManager(client);
+
+        } catch (Exception ex) {
+            logger.info("no service available!!");
+        }
+    }
+
+      public static void main (String[] args) throws ParseException {
 
         final Options options = new Options();
         final Option testMode = Option.builder().longOpt("testMode").build();
