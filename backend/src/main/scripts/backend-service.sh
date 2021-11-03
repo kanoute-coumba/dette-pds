d2c2345db5d00aaf557990e5c3c9f9f83d915d4b
kanoute-coumba <kanoute.coumba@gmail.com>
2021-11-03 18:22:40 +0100

    update pom files

--- backend/src/main/scripts/backend-service.sh
+++ backend/src/main/scripts/backend-service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd ../../../target
-exec java -jar backend-1.0-SNAPSHOT-jar-with-dependencies.jar $*
+#exec java -jar backend-1.0-SNAPSHOT-jar-with-dependencies.jar $*
+exec java -jar SmartCityAppServer-1.0-SNAPSHOT-jar-with-dependencies.jar $*
